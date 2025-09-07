4d2686d5ab0df8ee652a4e12c0de71ddfc0462d0
ryuring <egashira@catchup.co.jp>
2025-09-07 13:22:01 +0900

    ログを追加

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,8 +1,10 @@
 <?php
 
+use Cake\Core\Configure;
 use Cake\Log\Log;
 use Psr\Log\LogLevel;
 
+Log::setConfig(Configure::consume('Log'));
 Log::write(LogLevel::INFO, $_SERVER['REQUEST_URI'], 'mcp');
 if(!empty($_POST)) {
     Log::write(LogLevel::INFO, json_encode($_POST, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES), 'mcp');
