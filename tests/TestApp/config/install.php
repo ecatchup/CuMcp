5a2cd784a290a1db208180ce7935f4c6140633c5
ryuring <egashira@catchup.co.jp>
2025-09-12 19:05:25 +0900

    CIを調整

--- tests/TestApp/config/install.php
+++ tests/TestApp/config/install.php
@@ -4,7 +4,7 @@ return [
     'Datasources.default' => [
         'className' => 'Cake\\Database\\Connection',
         'driver' => 'Cake\\Database\\Driver\\Mysql',
-        'host' => 'cu-db',
+        'host' => '127.0.0.1',
         'port' => '3306',
         'username' => 'root',
         'password' => 'root',
@@ -18,7 +18,7 @@ return [
     'Datasources.test' => [
         'className' => 'Cake\\Database\\Connection',
         'driver' => 'Cake\\Database\\Driver\\Mysql',
-        'host' => 'cu-db',
+        'host' => '127.0.0.1',
         'port' => '3306',
         'username' => 'root',
         'password' => 'root',
