cee9f48906c827e87e6fe1bb3c9bcd3bc9de47ca
ryuring <egashira@catchup.co.jp>
2025-09-16 16:51:11 +0900

    インストーラーにbaserCMSのバージョン要件を追加

--- config.php
+++ config.php
@@ -1,8 +1,14 @@
 <?php
 declare(strict_types=1);
+
+use BaserCore\Utility\BcUtil;
+
 $message = [];
 if(!is_writable(CONFIG)) $message[] = CONFIG . ' に書き込み権限がありません。インストールする前に書き込み権限を与えてください。';
 if(!is_writable(CONFIG . '.env')) $message[] = CONFIG . '.env に書き込み権限がありません。インストールする前に書き込み権限を与えてください。';
+if(BcUtil::verpoint('5.1.10') > BcUtil::verpoint(BcUtil::getVersion())) {
+    $message[] = 'baserCMSのバージョンが5.1.10未満です。baserCMSを5.1.10以上にアップデートしてからインストールしてください。';
+}
 $message[] = 'インストール時には、認証必要領域の Web API（baser Admin Api）を有効を有効化します。';
 
 return [
